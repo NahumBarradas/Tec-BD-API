ec2db65e86e7c7d100f38eb57fe3f3393384d007
Nahum Barradas Vazquez <64337980+NahumBarradas@users.noreply.github.com>
2023-03-20 21:06:33 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,10 +9,10 @@ module.exports = {
     deploy: {
         "production": {
             "user": "nahum",
-            "host": "192.168.10.80",
+            "host": "192.168.56.100",
             "ref": "origin/master",
             "repo": "https://github.com/NahumBarradas/Tec-BD-API.git",
-            "path": "/home/nahum/deploy",
+            "path": "/home/server/deploy",
             "pre-setup": "pwd",
             "pre-deploy-local": "echo 'This is a local executed command'",
             "post-deploy": "cp../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production"
